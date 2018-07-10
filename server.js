301bd82ac3ac82278c0a67373b6e601604638094
Anthony Scotti <anthony.m.scotti@gmail.com>
2018-07-10 11:35:44 -0400

    Database code clean up and added morgan for logging

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 const express = require('express')
-const shortener = require('./lib/shortener')
+const morgan = require('morgan')
+
+const db = require('./lib/database')
+const shortener = require('./lib/shortener')(db)
 const bodyParser = require('body-parser')
 
 const app = express()
 
+app.use(morgan('tiny'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/get/:id', (req, res) => {
-  shortener.getURL(req.params.id).then((url) => {
-    res.json({
-      key: req.params.id,
-      url: url
-    })
+  shortener.getURL(req.params.id).then(url => {
+    res.json({ key: req.params.id, url: url })
   }).catch(() => {
     res.status(404).send({ error: `Key ${req.params.id} not found in database` })
   })
 })
 
 app.get('/:id', (req, res) => {
-  shortener.getURL(req.params.id).then((url) => {
+  shortener.getURL(req.params.id).then(url => {
     res.redirect(url)
   }).catch(() => {
     res.status(404).send({ error: `Key ${req.params.id} not found in database` })
@@ -26,16 +27,22 @@ app.get('/:id', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-  shortener.addURL(req.body.url).then((key) => {
-    res.json({
-      key: key,
-      url: req.body.url
-    })
+  shortener.addURL(req.body.url).then(key => {
+    res.json({ key: key, url: req.body.url })
   }).catch(() => {
     res.status(500).send({ error: 'Error creating URL' })
   })
 })
 
+app.delete('/:id', (req, res) => {
+  shortener.getURL(req.params.id).then(url => {
+    shortener.delURL(req.params.id)
+    res.json({ key: req.params.id, url: url })
+  }).catch(() => {
+    res.status(404).send({ error: `Key ${req.params.id} not found in database` })
+  })
+})
+
 const port = process.env.PORT || 8000
 app.listen(port)
-console.log(`Listen on port ${port}`)
+console.log(`Server running on port ${port}`)
