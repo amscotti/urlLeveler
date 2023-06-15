11906e7b1e4bbc693b03ade4489f29abbf66f628
Anthony Scotti <anthony.m.scotti@gmail.com>
2023-06-14 21:55:03 -0400

    Update to ES Modules

--- server.js
+++ server.js
@@ -1,40 +1,40 @@
-const express = require("express");
-const { createHandler } = require("graphql-http/lib/use/express");
-const morgan = require("morgan");
-const Database = require("./lib/database.js");
-const Shortener = require("./lib/shortener.js");
-const apiModule = require("./lib/api.js");
-const schemaModule = require("./lib/schema.js");
+import express from 'express'
+import { createHandler } from 'graphql-http/lib/use/express'
+import morgan from 'morgan'
+import { Database } from './lib/database.js'
+import { Shortener } from './lib/shortener.js'
+import { apiModule } from './lib/api.js'
+import { schemaModule } from './lib/schema.js'
 
-const database = new Database();
-const shortener = new Shortener(database);
+const database = new Database()
+const shortener = new Shortener(database)
 
-const apiRouter = apiModule(shortener);
-const schema = schemaModule(shortener);
+const apiRouter = apiModule(shortener)
+const schema = schemaModule(shortener)
 
-const app = express();
+const app = express()
 
 // Logging
-app.use(morgan("tiny"));
+app.use(morgan('tiny'))
 
 // REST API
-app.use("/api", apiRouter);
+app.use('/api', apiRouter)
 
 // GraphQL API
-app.use("/graphql", createHandler({ schema }));
+app.use('/graphql', createHandler({ schema }))
 
 // Based URL for redirects
-app.get("/:id", async (req, res) => {
+app.get('/:id', async (req, res) => {
   try {
-    const { url } = await shortener.getURL(req.params.id);
-    res.redirect(url);
+    const { url } = await shortener.getURL(req.params.id)
+    res.redirect(url)
   } catch (err) {
     res
       .status(404)
-      .send({ error: `Key ${req.params.id} not found in database` });
+      .send({ error: `Key ${req.params.id} not found in database` })
   }
-});
+})
 
-const port = process.env.PORT || 8000;
-app.listen(port);
-console.log(`Server running on port ${port}`);
+const port = process.env.PORT || 8000
+app.listen(port)
+console.log(`Server running on port ${port}`)
